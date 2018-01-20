58efb4d3034de51718d243126c3e1b686809abd1
Erwin <e.verdonk@gmail.com>
2018-01-20 11:14:12 +0100

    Deploy task 2/2

--- deploy.js
+++ deploy.js
@@ -1,46 +1,64 @@
-const AWS = require('aws-sdk');
-AWS.config.update({ region: 'eu-west-1' });
-
-const fs = require('fs');
-const cf = new AWS.CloudFormation();
-const cfTemplate = fs.readFileSync('cloudformation.yaml');
-
-cf.createStack({
-  StackName: '',
-  TemplateBody: cfTemplate.toString(),
-  Capabilities: ['CAPABILITY_IAM'],
-  Parameters: [
-    {
-      ParameterKey: 'Subdomain',
-      ParameterValue: '',
-    },
-    {
-      ParameterKey: 'PrimaryDomain',
-      ParameterValue: '',
-    },
-  ],
-})
-.promise()
-.catch(_ => {
-  if (_.code === 'AlreadyExistsException') {
-    cf.updateStack({
-      StackName: '',
-      TemplateBody: cfTemplate.toString(),
-      Capabilities: ['CAPABILITY_IAM'],
-      Parameters: [
-        {
-          ParameterKey: 'Subdomain',
-          ParameterValue: '',
-        },
-        {
-          ParameterKey: 'PrimaryDomain',
-          ParameterValue: '',
-        },
-      ],
-    })
-    .promise()
-    .catch(_ => console.error(_));
-  } else {
-    console.error(_);
+require('aws-cfn-custom-resource-s3-empty-bucket').default.then(_ => {
+  if(process.argv.length < 3){
+    throw new Error('No domain argument provided')
   }
+
+  const domainSplit = process.argv[2].split(/\.(?=[^.]+\.[^.]+$)/).filter(_ => !!_);
+
+  if(domainSplit.length < 2){
+    throw new Error('Invalid domain argument provided')
+  }
+
+  const domain = domainSplit.join('.');
+  const subdomain = domainSplit[0];
+  const primaryDomain = domainSplit[1];
+  const stackName = domain.replace(/[^a-z0-9-]/ig, '-');
+
+  const AWS = require('aws-sdk');
+  AWS.config.update({ region: 'eu-west-1' });
+
+  const fs = require('fs');
+  const cf = new AWS.CloudFormation();
+  const cfTemplate = fs.readFileSync('cloudformation.yaml');
+
+  cf.createStack({
+    StackName: stackName,
+    TemplateBody: cfTemplate.toString(),
+    Capabilities: ['CAPABILITY_IAM'],
+    Parameters: [
+      {
+        ParameterKey: 'Subdomain',
+        ParameterValue: subdomain,
+      },
+      {
+        ParameterKey: 'PrimaryDomain',
+        ParameterValue: primaryDomain,
+      },
+    ],
+  })
+  .promise()
+  .catch(_ => {
+    if (_.code === 'AlreadyExistsException') {
+      cf.updateStack({
+        StackName: stackName,
+        TemplateBody: cfTemplate.toString(),
+        Capabilities: ['CAPABILITY_IAM'],
+        Parameters: [
+          {
+            ParameterKey: 'Subdomain',
+            ParameterValue: subdomain,
+          },
+          {
+            ParameterKey: 'PrimaryDomain',
+            ParameterValue: primaryDomain,
+          },
+        ],
+      })
+      .promise()
+      .catch(_ => console.error(_));
+    } else {
+      console.error(_);
+    }
+  });
+
 });
