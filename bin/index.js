4ed95a9e5c3059cf36a039415a0c8845b758e80e
Erwin <e.verdonk@gmail.com>
2018-01-21 20:08:46 +0100

    Change the way we export deploy logic to make it more usable in other applications

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,68 @@
 exports.deploy = (domain) => {
-  process.argv.push(domain);
-  return require('./../tasks/deploy').default;
+  if(!domain){
+    throw new Error('No domain argument provided')
+  }
+
+  const domainSplit = domain.split(/\.(?=[^.]+\.[^.]+$)/).filter(_ => !!_);
+
+  if(domainSplit.length < 2){
+    throw new Error('Invalid domain argument provided')
+  }
+
+  const subdomain = domainSplit[0];
+  const primaryDomain = domainSplit[1];
+  const stackName = domain.replace(/[^a-z0-9-]/ig, '-');
+
+  const AWS = require('aws-sdk');
+  AWS.config.update({ region: 'eu-west-1' });
+
+
+  return require('aws-cfn-custom-resource-s3-empty-bucket')
+    .deploy()
+    .then(_ => {
+      const fs = require('fs');
+      const cf = new AWS.CloudFormation();
+      const cfTemplate = fs.readFileSync(__dirname + '/../lib/cloudformation.yaml');
+
+      return cf.createStack({
+        StackName: stackName,
+        TemplateBody: cfTemplate.toString(),
+        Capabilities: ['CAPABILITY_IAM'],
+        Parameters: [
+          {
+            ParameterKey: 'Subdomain',
+            ParameterValue: subdomain,
+          },
+          {
+            ParameterKey: 'PrimaryDomain',
+            ParameterValue: primaryDomain,
+          },
+        ],
+      })
+      .promise()
+      .catch(_ => {
+        if (_.code === 'AlreadyExistsException') {
+          cf.updateStack({
+            StackName: stackName,
+            TemplateBody: cfTemplate.toString(),
+            Capabilities: ['CAPABILITY_IAM'],
+            Parameters: [
+              {
+                ParameterKey: 'Subdomain',
+                ParameterValue: subdomain,
+              },
+              {
+                ParameterKey: 'PrimaryDomain',
+                ParameterValue: primaryDomain,
+              },
+            ],
+          })
+          .promise()
+          .catch(_ => console.error(_));
+        } else {
+          console.error(_);
+        }
+      });
+    }
+  );
 }
