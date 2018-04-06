735e7f9185924036dc396d544efdcc9e46ee38f4
Erwin Verdonk <erwin@instantmagazine.com>
2018-04-06 14:37:32 +0200

    Refactor to make more robust and better readable code

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,45 @@
 const AWS = require('aws-sdk');
+const fs = require('fs');
+const cfTemplate = fs.readFileSync(__dirname + '/../lib/cloudformation.yaml');
+
+const createWait = (cf, stackName, status) => () => cf.waitFor(status, {
+  StackName: stackName
+}).promise();
+
+const deployS3EmptyBucketCfnCustomResource = cf => {
+  // Deploy S3EmptyBucket CloudFormation Custom Resource to make sure
+  // that an S3 bucket is emptied before deletion. Otherwise the S3
+  // bucket cannot be deleted by CloudFormation.
+  return require('aws-cfn-custom-resource-s3-empty-bucket').deploy()
+    // Then wait for the CloudFormation Custom Resource to be
+    // available.
+    .then(_ => _
+      .wait()
+      .catch(e => {
+        throw e.originalError
+          ? new Error(JSON.stringify(e.originalError))
+          : new Error(JSON.stringify(e));
+      })
+    )
+}
+
+const checkStackExists = (cf, stackName) => {
+  return cf.listStacks()
+    .promise()
+    .then(_ => (
+      _.StackSummaries.some(_ => (
+        _.StackName === stackName &&
+        // The status 'DELETE_COMPLETE' should be considered as not existing
+        _.StackStatus !== 'DELETE_COMPLETE'
+      ))
+    ));
+}
+
+const getStackStatus = (cf, stackName) => {
+  return cf.describeStacks({ StackName: stackName })
+  .promise()
+  .then(result => result.Stacks[0].StackStatus)
+};
 
 exports.deploy = args => {
   const region = args.region ? args.region : process.env.AWS_DEFAULT_REGION;
@@ -22,62 +63,74 @@ exports.deploy = args => {
   const subdomain = domainSplit[0];
   const primaryDomain = domainSplit[1];
   const stackName = args.domain.replace(/[^a-z0-9-]/gi, '-');
+  const stackParams = {
+    StackName: stackName,
+    TemplateBody: cfTemplate.toString(),
+    Capabilities: ['CAPABILITY_IAM'],
+    Parameters: [
+      {
+        ParameterKey: 'Subdomain',
+        ParameterValue: subdomain,
+      },
+      {
+        ParameterKey: 'PrimaryDomain',
+        ParameterValue: primaryDomain,
+      },
+      {
+        ParameterKey: 'HasOAI',
+        ParameterValue: hasOAI.toString(),
+      },
+      {
+        ParameterKey: 'LambdaEdgeArn',
+        ParameterValue: lambdaEdgeArn ? lambdaEdgeArn : '',
+      },
+    ],
+  };
 
   const cf = new AWS.CloudFormation({ region });
 
-  return require('aws-cfn-custom-resource-s3-empty-bucket')
-    .deploy()
-    .then(_ => cf.waitFor(_.pendingResourceState, { StackName: _.stackName }).promise())
-    .then(_ => {
-      const fs = require('fs');
-      const cfTemplate = fs.readFileSync(__dirname + '/../lib/cloudformation.yaml');
-
-      const stackParams = {
-        StackName: stackName,
-        TemplateBody: cfTemplate.toString(),
-        Capabilities: ['CAPABILITY_IAM'],
-        Parameters: [
-          {
-            ParameterKey: 'Subdomain',
-            ParameterValue: subdomain,
-          },
-          {
-            ParameterKey: 'PrimaryDomain',
-            ParameterValue: primaryDomain,
-          },
-          {
-            ParameterKey: 'HasOAI',
-            ParameterValue: hasOAI.toString(),
-          },
-          {
-            ParameterKey: 'LambdaEdgeArn',
-            ParameterValue: lambdaEdgeArn ? lambdaEdgeArn : '',
-          },
-        ],
-      };
-
-      console.log(`Create or update of CloudFormation stack '${stackName}' started`);
-
-      return cf
-        .createStack(stackParams)
-        .promise()
-        .then(_ => ({ stackName, pendingResourceState: 'stackCreateComplete' }))
-        .catch(_ => {
-          if (_.code === 'AlreadyExistsException') {
-            return cf
-              .updateStack(stackParams)
-              .promise()
-              .then(_ => ({ stackName, pendingResourceState: 'stackUpdateComplete' }))
-              .catch(_ => {
-                if (_.message.toLowerCase().indexOf('no update') > -1) {
-                  return { stackName, pendingResourceState: 'stackExists' };
-                } else {
-                  throw _;
-                }
-              });
-          } else {
-            throw _;
-          }
-        });
+  return deployS3EmptyBucketCfnCustomResource(cf)
+    .then(() => checkStackExists(cf, stackParams.StackName))
+    .then(alreadyExists => {
+      if(alreadyExists){
+        return getStackStatus(cf, stackParams.StackName)
+          .then(status => {
+            switch(status){
+              case 'CREATE_COMPLETE':
+              case 'UPDATE_COMPLETE':
+              case 'ROLLBACK_COMPLETE':
+              case 'UPDATE_ROLLBACK_COMPLETE':
+                // Start the update of the stack
+                cf.updateStack(stackParams)
+                  .promise()
+                  .then(_ => ({
+                    wait: createWait(cf, stackName, 'stackUpdateComplete')
+                  }))
+                  .catch(_ => {
+                    if (_.message.toLowerCase().indexOf('no update') > -1) {
+                      return {
+                        wait: createWait(cf, stackName, 'stackExists')
+                      }
+                    } else {
+                      throw _;
+                    }
+                  });
+                break;
+              case 'CREATE_IN_PROGRESS':
+              case 'UPDATE_IN_PROGRESS':
+                console.log(`AWS CloudFront Hosting is in progress to be created or updated`);
+                break;
+              default:
+                throw new Error(`Stack '${stackParams.StackName}' is in blocking state: ${status}`)
+            }
+          })
+      } else {
+        // Start the creation of the stack
+        return cf.createStack(stackParams)
+          .promise()
+          .then(_ => ({
+            wait: createWait(cf, stackName, 'stackCreateComplete')
+          }));
+      }
     });
 };
